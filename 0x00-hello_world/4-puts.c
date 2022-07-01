1b9597a7ced325daceb70fc31e437f9d85527e7f
1katuramu <edgarkaturamu@gmail.com>
2022-06-30 17:39:00 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
-#include <stdio.h>
+#include <stdio.>
+/**
+ * main -Entry point
+ *
+ * Description: 'Hello, puts'
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
