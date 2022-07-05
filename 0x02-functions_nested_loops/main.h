4965815d3c545b6cfce6a96ee2f1e43c907f7c3a
1katuramu <edgarkaturamu@gmail.com>
2022-07-05 13:47:19 -0700

    _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,21 +1,23 @@
-#include "main.h"
-
 /**
- * main - prints putchar
- * Return: always 0
+ * main - entry point
+ *
+ * Print holberton
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	int _putchar(char c);
 
-return (0);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }
-
